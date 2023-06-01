9be983df60001d0c29897f42ac01132322be77b3
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-06-01 14:18:11 +0700

    Added phpstan & phpunit/phpunit

---
+++ tests/FilesystemGenericTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+/**
+ * 📁 PHP library for file and directory management. Provides basic methods for the filesystem.
+ *
+ * @author belomaxorka
+ * @license MIT (https://github.com/belomaxorka/filesystem/blob/main/LICENSE)
+ * @link https://github.com/belomaxorka/filesystem
+ */
+
+use PHPUnit\Framework\TestCase;
+
+final class FilesystemGenericTest extends TestCase
+{
+}
